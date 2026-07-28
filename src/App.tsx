126db2371b866194f7415f6e9ca21c00a5500730
itshakemd <itshakemd@gmail.com>
2026-07-28 13:21:33 +0300

    feat: add GitHub connection card

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
-export default function App() {
-  return <div>GitHub readme</div>;
+import ConnectCard from "./components/ConnectCard";
+export default function App({ openSettings }: { openSettings: () => void }) {
+  return <div className="github-readme-root"><ConnectCard openSettings={openSettings} /></div>;
 }
